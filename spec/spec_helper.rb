c2bd2e046c718d62053e1b6ea4815802121c8639
github username <github email address>
2019-02-25 13:45:33 -0500

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,15 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  require 'rails_helper'
+
+# describe 'new post' do
+#   it 'ensures that the form route works with the /new action' do
+#     visit new_post_path
+#     expect(page.status_code).to eq(200)
+#   end
+# end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
